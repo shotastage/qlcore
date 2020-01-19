e8acc8d9286baa299e4d25cf25ca59c53c3d27cc
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-19 11:18:58 +0900

    revise

---
+++ dqncatp/environment.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+
+class CATPEnvironment:
+
+    def __init__(self):
+        pass
+
+    def commit(self):
+        pass
+
+
+    def _capacity_validate(self, route_id):
+        
+        if True:
+            return -10
