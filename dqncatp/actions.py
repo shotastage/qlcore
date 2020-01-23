eebb515edf2357085e324fafde801a7eb11eb30b
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-23 22:17:51 +0900

    revise

---
+++ dqncatp/actions.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+
+
+class CATPAction:
+
+    def __init__(self):
+        pass
+
+    def commit(self):
+        pass
+
+    def action(self, act_obj):
+        pass
+
+    def _capacity_validate(self, route_id):
+        
+        if True:
+            return -10
